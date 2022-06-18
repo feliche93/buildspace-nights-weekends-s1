0445ba72dfc5bd4019c7c1160f41bb9a01c3b74b
Felix Vemmer <felix.vemmer@gmail.com>
2022-06-18 16:57:43 +0200

    feat: next-app up

---
+++ next-app/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <h1 className='text-3xl font-bold underline'>
+      Buildspace Nights & Weekends
+    </h1>
+  );
+};
+
+export default Home;
